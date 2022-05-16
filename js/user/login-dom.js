ffc2008d5ac7ef9d049e6a4e479206d2fb80f936
pau <jethro.riosa@gmail.com>
2022-05-16 17:54:04 +0800

    able to create load data in index.html

--- js/user/login-dom.js
+++ js/user/login-dom.js
@@ -1,11 +1,18 @@
 import { createUser, login } from "./index.js";
+import { loadData } from "../helper.js";
 
+let loadBtn = document.querySelector("#load-data-btn");
 const loginBtn = document.querySelector("#login-btn");
 const loginSection = document.querySelector("section.login");
 const adminDashboardSection = document.querySelector("section.admin-dashboard");
 const userDashboardSection = document.querySelector("section.user-dashboard");
 const mobile = document.querySelector("#mobile");
 const password = document.querySelector("#password");
+
+loadBtn.onclick = function (e) {
+  e.preventDefault();
+  loadData();
+};
 
 loginBtn.onclick = function (e) {
   e.preventDefault();
