a0f9a11d6b2a30f7f1e630f481dbf958730328a2
pau <jethro.riosa@gmail.com>
2022-05-17 18:53:00 +0800

    work in progress: withdraw, deposit, and transfer

--- js/user/admindashboard-dom.js
+++ js/user/admindashboard-dom.js
@@ -20,12 +20,7 @@ function displayUsers(users) {
             <td>${u.fullname}</td>
             <td>${total.balance}</td>
             <td>
-              <select>
-                 <option value="" selected>action</option> 
-                 <option value="withdraw">withdraw</option> 
-                 <option value="deposit">deposit</option> 
-                 <option value="transfer">transfer</option> 
-              </select>
+                <a href="#withdraw-modal" class="withdraw-modal-btn">withdraw</a>
             </td>
           </tr>
     `;
@@ -34,6 +29,29 @@ function displayUsers(users) {
   tableBody.innerHTML = result.join("").toString();
 }
 
-(function () {
-  displayUsers(users);
-})();
+// Get the modal
+// var modal = document.getElementById("withdraw-modal");
+
+// // Get the button that opens the modal
+// var btn = document.querySelector(".withdraw-modal-btn");
+// console.log(btn);
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// };
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = (function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// })
+displayUsers(users);
